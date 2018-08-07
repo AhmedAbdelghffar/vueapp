b71a6ce57f9bec513a612242b2d5a1109f0b1dc8
AhmedAbdelghffar <ahmedsoud3@gmail.com>
2018-08-07 13:38:07 +0200

    Vue Introduction

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1> this is about {{name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      name: this.$route.params.name
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

af2e55da5e2e298dae1bc946416e3d6b5fc27beb
AhmedAbdelghffar <ahmedsoud3@gmail.com>
2018-08-07 13:16:12 +0200

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="Home">
+    <input type="text" v-on:input="changeTitle">
+    <h1>{{ msg }}</h1>
+    <hr>
+    <form @submit.prevent="addSkill">
+      <input type="text" v-model="skill" v-validate="'min:5'" name="skill">
+    <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+    <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+    </transition>
+    </form>
+
+    <ul>
+      <transition-group name="list-push" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
+      <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+      </transition-group>
+    </ul> 
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      skill: '',
+      skills: [
+        { "skill" : "vue.js"},
+        { "skill" : "WordPress" }
+      ]
+    }
+  },
+  methods: {
+    changeTitle: function(event){
+      this.msg = event.target.value;
+    },
+    addSkill:function() {
+      this.skills.push({skill: this.skill});
+      this.skill = '';
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./hello.css" scoped>
+
+</style>
